4d193e77f74db67b3414a038fda3efb1ee620d41
Anwarul07 <mdanwarul064@gmail.com>
2024-03-20 17:35:48 +0530

    update whearther.js

--- wheather.js
+++ wheather.js
@@ -9,23 +9,30 @@ const options = {
 };
 
 
+const getWheather = (city)=>{
+	cityName.innerHTML=city;
 
+	fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=` +city, options)
+		.then(response => response.json())
+		.then((response) => {
 
-fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle`, options)
-	.then(response => response.json())
-	.then((response) => {
+			console.log(response)
+			cloud_pct.innerHTML = response.cloud_pct
+			feels_like.innerHTML = response.feels_like
+			humidity.innerHTML = response.humidity
+			max_temp.innerHTML = response.max_temp
+			min_temp.innerHTML = response.min_temp
+			sunrise.innerHTML = response.sunrise
+			sunset.innerHTML = response.sunset
+			temp.innerHTML = response.temp
+			wind_degrees.innerHTML = response.wind_degrees
+			wind_speed.innerHTML = response.wind_speed
 
-		console.log(response)
-		cloud_pct.innerHTML = response.cloud_pct
-		feels_like.innerHTML = response.feels_like
-		humidity.innerHTML = response.humidity
-		max_temp.innerHTML = response.max_temp
-		min_temp.innerHTML = response.min_temp
-		sunrise.innerHTML = response.sunrise
-		sunset.innerHTML = response.sunset
-		temp.innerHTML = response.temp
-		wind_degrees.innerHTML = response.wind_degree
-		wind_speed.innerHTML = response.wind_speed
-
-	})
-	.catch(err => console.error(err));
+		})
+		.catch(err => console.error(err));
+}
+submit.addEventListener("click", (e) => {
+	e.preventDefault()
+getWheather(city.value);
+})
+getWheather("Delhi")
