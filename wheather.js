85037a8881471c3933bdbe3d9a9865eb0e8ec179
Anwarul07 <mdanwarul064@gmail.com>
2024-03-20 16:24:50 +0530

    Update wheather.js

--- wheather.js
+++ wheather.js
@@ -1,5 +1,5 @@
 console.log("hello world");
-const url = 'https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const url = 'https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?cit.innerHTMLy=Seattle';
 const options = {
 	method: 'GET',
 	headers: {
@@ -12,6 +12,20 @@ const options = {
 
 
 fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle`, options)
-.then(response => response.json())
-.then(response => console.log(response))
-.catch(err => console.error(err));
+	.then(response => response.json())
+	.then((response) => {
+
+		console.log(response)
+		cloud_pct.innerHTML = response.cloud_pct
+		feels_like.innerHTML = response.feels_like
+		humidity.innerHTML = response.humidity
+		max_temp.innerHTML = response.max_temp
+		min_temp.innerHTML = response.min_temp
+		sunrise.innerHTML = response.sunrise
+		sunset.innerHTML = response.sunset
+		temp.innerHTML = response.temp
+		wind_degrees.innerHTML = response.wind_degree
+		wind_speed.innerHTML = response.wind_speed
+
+	})
+	.catch(err => console.error(err));
