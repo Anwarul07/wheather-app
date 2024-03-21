632a6b5d46b8ac11efd56848fa85e49afef29643
Anwarul07 <mdanwarul064@gmail.com>
2024-03-21 12:14:50 +0530

    Update Whether.js

--- wheather.js
+++ wheather.js
@@ -9,30 +9,86 @@ const options = {
 };
 
 
-const getWheather = (city)=>{
-	cityName.innerHTML=city;
+const getWheather = (city) => {
+	cityName.innerHTML = city;
 
-	fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=` +city, options)
+	fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=` + city, options)
 		.then(response => response.json())
 		.then((response) => {
 
 			console.log(response)
-			cloud_pct.innerHTML = response.cloud_pct
+			// cloud_pct.innerHTML = response.cloud_pct
 			feels_like.innerHTML = response.feels_like
 			humidity.innerHTML = response.humidity
+			humidity2.innerHTML = response.humidity
+
 			max_temp.innerHTML = response.max_temp
 			min_temp.innerHTML = response.min_temp
 			sunrise.innerHTML = response.sunrise
 			sunset.innerHTML = response.sunset
 			temp.innerHTML = response.temp
+			temp2.innerHTML = response.temp
 			wind_degrees.innerHTML = response.wind_degrees
 			wind_speed.innerHTML = response.wind_speed
+			wind_speed2.innerHTML = response.wind_speed
+
 
 		})
 		.catch(err => console.error(err));
 }
 submit.addEventListener("click", (e) => {
 	e.preventDefault()
-getWheather(city.value);
+	getWheather(city.value);
 })
-getWheather("Delhi")
+getWheather("Delhi");
+
+
+
+
+fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=delhi`, options)
+	.then(response => response.json())
+	.then((response) => {
+
+		console.log(response)
+		feels_like3.innerHTML = response.feels_like
+		humidity3.innerHTML = response.humidity
+		humidity3.innerHTML = response.humidity
+
+		max_temp3.innerHTML = response.max_temp
+		min_temp3.innerHTML = response.min_temp
+		sunrise3.innerHTML = response.sunrise
+		sunset3.innerHTML = response.sunset
+		temp3.innerHTML = response.temp
+		wind_degrees3.innerHTML = response.wind_degrees
+		wind_speed3.innerHTML = response.wind_speed
+		wind_speed3.innerHTML = response.wind_speed
+
+
+	})
+	.catch(err => console.error(err));
+getWheather("Delhi");
+
+
+
+
+fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=kolkata`, options)
+	.then(response => response.json())
+	.then((response) => {
+
+		console.log(response)
+		feels_like4.innerHTML = response.feels_like
+		humidity4.innerHTML = response.humidity
+		humidity4.innerHTML = response.humidity
+		max_temp4.innerHTML = response.max_temp
+		min_temp4.innerHTML = response.min_temp
+		sunrise4.innerHTML = response.sunrise
+		sunset4.innerHTML = response.sunset
+		temp4.innerHTML = response.temp
+		wind_degrees4.innerHTML = response.wind_degrees
+		wind_speed4.innerHTML = response.wind_speed
+		wind_speed4.innerHTML = response.wind_speed
+
+
+	})
+	.catch(err => console.error(err));
+getWheather("Kolkata");
