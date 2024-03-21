9d716b623c692b844ffa2d40c6cac6e0ba816f71
Anwarul07 <mdanwarul064@gmail.com>
2024-03-21 19:49:12 +0530

    Update wheather.js

--- wheather.js
+++ wheather.js
@@ -9,6 +9,7 @@ const options = {
 };
 
 
+//All Cities Wheather
 const getWheather = (city) => {
 	cityName.innerHTML = city;
 
@@ -17,7 +18,6 @@ const getWheather = (city) => {
 		.then((response) => {
 
 			console.log(response)
-			// cloud_pct.innerHTML = response.cloud_pct
 			feels_like.innerHTML = response.feels_like
 			humidity.innerHTML = response.humidity
 			humidity2.innerHTML = response.humidity
@@ -38,14 +38,15 @@ const getWheather = (city) => {
 }
 submit.addEventListener("click", (e) => {
 	e.preventDefault()
-	getWheather(city.value);
+	getWheather(cityName.value);
 })
-getWheather("Delhi");
+//By Default Delhi
+getWheather("delhi");
 
 
 
-
-fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=delhi`, options)
+//Shanghai City Wheather
+fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=shanghai`, options)
 	.then(response => response.json())
 	.then((response) => {
 
@@ -66,11 +67,11 @@ fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=delhi`, option
 
 	})
 	.catch(err => console.error(err));
-getWheather("Delhi");
+getWheather("Shanghai");
 
 
 
-
+//Kolkata City Wheather
 fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=kolkata`, options)
 	.then(response => response.json())
 	.then((response) => {
@@ -92,3 +93,104 @@ fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=kolkata`, opti
 	})
 	.catch(err => console.error(err));
 getWheather("Kolkata");
+
+
+
+//Bihar City Wheather
+fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=bihar`, options)
+	.then(response => response.json())
+	.then((response) => {
+
+		console.log(response)
+		feels_like5.innerHTML = response.feels_like
+		humidity5.innerHTML = response.humidity
+		humidity5.innerHTML = response.humidity
+		max_temp5.innerHTML = response.max_temp
+		min_temp5.innerHTML = response.min_temp
+		sunrise5.innerHTML = response.sunrise
+		sunset5.innerHTML = response.sunset
+		temp5.innerHTML = response.temp
+		wind_degrees5.innerHTML = response.wind_degrees
+		wind_speed5.innerHTML = response.wind_speed
+		wind_speed5.innerHTML = response.wind_speed
+
+
+	})
+	.catch(err => console.error(err));
+getWheather("Bihar");
+
+
+//Bhopal City Wheather
+fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=bhopal`, options)
+	.then(response => response.json())
+	.then((response) => {
+
+		console.log(response)
+		feels_like6.innerHTML = response.feels_like
+		humidity6.innerHTML = response.humidity
+		humidity6.innerHTML = response.humidity
+		max_temp6.innerHTML = response.max_temp
+		min_temp6.innerHTML = response.min_temp
+		sunrise6.innerHTML = response.sunrise
+		sunset6.innerHTML = response.sunset
+		temp6.innerHTML = response.temp
+		wind_degrees6.innerHTML = response.wind_degrees
+		wind_speed6.innerHTML = response.wind_speed
+		wind_speed6.innerHTML = response.wind_speed
+
+
+	})
+	.catch(err => console.error(err));
+getWheather("Bhopal");
+
+
+
+//Boston City Wheather
+fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=boston`, options)
+	.then(response => response.json())
+	.then((response) => {
+
+		console.log(response)
+		feels_like7.innerHTML = response.feels_like
+		humidity7.innerHTML = response.humidity
+		humidity7.innerHTML = response.humidity
+		max_temp7.innerHTML = response.max_temp
+		min_temp7.innerHTML = response.min_temp
+		sunrise7.innerHTML = response.sunrise
+		sunset7.innerHTML = response.sunset
+		temp7.innerHTML = response.temp
+		wind_degrees7.innerHTML = response.wind_degrees
+		wind_speed7.innerHTML = response.wind_speed
+		wind_speed7.innerHTML = response.wind_speed
+
+
+	})
+	.catch(err => console.error(err));
+getWheather("boston");
+
+
+//Mumbai City Wheather
+fetch(`https:weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Mumbai`, options)
+	.then(response => response.json())
+	.then((response) => {
+
+		console.log(response)
+		feels_like8.innerHTML = response.feels_like
+		humidity8.innerHTML = response.humidity
+		humidity8.innerHTML = response.humidity
+		max_temp8.innerHTML = response.max_temp
+		min_temp8.innerHTML = response.min_temp
+		sunrise8.innerHTML = response.sunrise
+		sunset8.innerHTML = response.sunset
+		temp8.innerHTML = response.temp
+		wind_degrees8.innerHTML = response.wind_degrees
+		wind_speed8.innerHTML = response.wind_speed
+		wind_speed8.innerHTML = response.wind_speed
+
+
+	})
+	.catch(err => console.error(err));
+getWheather("Mumbai");
+
+
+
